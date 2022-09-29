adc11801cece0f1f94358415df01a0dc01628aa0
arseny-chebyshev <arseny.chebyshev@gmail.com>
2022-09-30 01:18:12 +0500

    исправление багов

--- client/utils.py
+++ client/utils.py
@@ -11,7 +11,7 @@ async def wait_for_order(bot, chat_id, order_id):
                          InlineKeyboardButton(text="Повторить", callback_data=f"callback_repeat{order.id}"),
                          InlineKeyboardButton(text="Сделать новый заказ", callback_data=f"callback_new")
                         )
-            await bot.send_message(chat_id=chat_id, text=f"""Заказ <strong>#{order_id}</stong> готов! 
+            await bot.send_message(chat_id=chat_id, text=f"""Заказ <strong>#{order_id}</strong> готов! 
 Его можно оплатить при получении.""", reply_markup=inline_kbd)
             break
         else:
